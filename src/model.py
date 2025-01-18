9bd7938f2f883361b35086513d3a8b42aaacfcd8
Pritisingh06 <142310439+Pritisingh06@users.noreply.github.com>
2025-01-18 17:04:30 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,16 @@
 
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def create_model(img_height, img_width, num_classes):
+    model = models.Sequential([
+        layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+        layers.Conv2D(128, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(1024, activation='relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    return model
